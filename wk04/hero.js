02c243ac60869003a48139308c1c70f58d97bab5
tjaskolka <63914143+tjaskolka@users.noreply.github.com>
2021-05-16 18:05:00 -0400

    start wk05

--- wk04/hero.js
+++ wk04/hero.js
@@ -7,12 +7,8 @@ function makeHero(event) {
     hero.name = form.heroName.value;
     hero.realName = form.realName.value;
     hero.age = form.age.value;
-    hero.powers = [];
-    for (let i=0; i < form.powers.length; i++) {
-        if (form.powers[i].checked) {
-            hero.powers.push(form.powers[i].value);
-        }
-    }
+    hero.powers = [...form.powers].filter(box =>
+        box.checked).map(box => box.value);
     hero.city = form.city.value;
     hero.origin = form.origin.value;
     alert(JSON.stringify(hero));
@@ -26,5 +22,30 @@ function validate(event) {
     if (firstLetter.toUpperCase() === 'X') {
         event.preventDefault();
         alert('Your name is not allowed to start with X!');
+    }
+}
+
+const label = form.querySelector('label');
+const error = document.createElement('div');
+error.classList.add('error');
+error.textContent = '! Your name is not allowed to start with X.';
+label.append(error);
+
+function validateInline() {
+    const heroName = this.value.toUpperCase();
+    if(heroName.startsWith('X')) {
+        error.style.display = 'block';
+    } else {
+        error.style.display = 'none';
+    }
+}
+
+form.heroName.addEventListener('keyup', disableSubmit, false);
+
+function disableSubmit(event) {
+    if(event.target.value === '') {
+        document.getElementById('submit').disabled = true;
+    } else {
+        document.getElementById('submit').disabled = false;
     }
 }
